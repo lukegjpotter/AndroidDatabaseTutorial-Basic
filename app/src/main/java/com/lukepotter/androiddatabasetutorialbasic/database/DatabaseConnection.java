2340692b6eead4316a6cd3c3e89f8c15e44646f3
Luke GJ Potter <lukegjpotter@gmail.com>
2015-03-02 23:00:34 +0000

    Step 3 Bonus: The other CRUD queries

--- app/src/main/java/com/lukepotter/androiddatabasetutorialbasic/database/DatabaseConnection.java
+++ app/src/main/java/com/lukepotter/androiddatabasetutorialbasic/database/DatabaseConnection.java
@@ -30,14 +30,9 @@ public class DatabaseConnection extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     // Table Name.
     private static final String TABLE_BLOGPOSTS = "blogposts";
-    // SQL Query for Deleting (dropping) the table.
-    private static final String DROP_BLOGPOSTS_TABLE = "DROP TABLE IF EXISTS " + TABLE_BLOGPOSTS;
     // Blog Post Table Columns, these map to the public fields on the BlogPost class.
     private static final String KEY_ID = "id";
     private static final String KEY_TITLE = "title";
-    // SQL Query for creating the table.
-    private static final String CREATE_BLOGPOSTS_TABLE = "CREATE TABLE " + TABLE_BLOGPOSTS + " ("
-            + KEY_ID + " INTEGER PRIMARY KEY, " + KEY_TITLE + " TEXT)";
     // Singleton Instance.
     private static DatabaseConnection ourInstance;
 
@@ -61,19 +56,28 @@ public class DatabaseConnection extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase database) {
+
+        // SQL Query for creating the table.
+        final String CREATE_BLOGPOSTS_TABLE = "CREATE TABLE " + TABLE_BLOGPOSTS + " ("
+                + KEY_ID + " INTEGER PRIMARY KEY, " + KEY_TITLE + " TEXT)";
         database.execSQL(CREATE_BLOGPOSTS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+
         // Drop the older table, if there's a newer table, and create the newer table.
         if (newVersion > oldVersion) {
+            // SQL Query for Deleting (dropping) the table.
+            final String DROP_BLOGPOSTS_TABLE = "DROP TABLE IF EXISTS " + TABLE_BLOGPOSTS;
             database.execSQL(DROP_BLOGPOSTS_TABLE);
+
             onCreate(database);
         }
     }
 
     public void addBlogPost(BlogPost blogPost) {
+
         SQLiteDatabase database = getWritableDatabase();
 
         ContentValues contentValues = new ContentValues();
@@ -103,5 +107,70 @@ public class DatabaseConnection extends SQLiteOpenHelper {
         database.close();
 
         return blogPosts;
+    }
+
+    public BlogPost getBlogPost(int id) {
+
+        BlogPost blogPost = null;
+
+        String selection = KEY_ID + "=?";
+        String[] selectionArgs = new String[]{String.valueOf(id)};
+        String limit = String.valueOf(1);
+
+        SQLiteDatabase database = getReadableDatabase();
+        Cursor cursor = database.query(
+                TABLE_BLOGPOSTS, null, selection, selectionArgs, null, null, null, limit);
+
+        if (cursor.moveToFirst()) {
+            blogPost = new BlogPost(
+                    Integer.parseInt(cursor.getString(cursor.getColumnIndex(KEY_ID))),
+                    cursor.getString(cursor.getColumnIndex(KEY_TITLE)));
+        }
+
+        cursor.close();
+        database.close();
+
+        return blogPost;
+    }
+
+    public void updateBlogPost(BlogPost updatedBlogPost) {
+
+        SQLiteDatabase database = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(KEY_ID, updatedBlogPost.getId());
+        contentValues.put(KEY_TITLE, updatedBlogPost.getTitle());
+
+        String whereClause = KEY_ID + "=?";
+        String[] whereArgs = new String[]{String.valueOf(updatedBlogPost.getId())};
+
+        database.update(TABLE_BLOGPOSTS, contentValues, whereClause, whereArgs);
+
+        database.close();
+    }
+
+    public void deleteBlogPost(BlogPost blogPostToDelete) {
+
+        String whereClause = KEY_ID + "=?";
+        String[] whereArgs = new String[]{String.valueOf(blogPostToDelete.getId())};
+
+        SQLiteDatabase database = getWritableDatabase();
+        database.delete(TABLE_BLOGPOSTS, whereClause, whereArgs);
+
+        database.close();
+    }
+
+    public int getBlogPostsCount() {
+
+        String[] columns = new String[]{KEY_ID};
+
+        SQLiteDatabase database = getReadableDatabase();
+        Cursor cursor = database.query(TABLE_BLOGPOSTS, columns, null, null, null, null, null);
+        int numberOfBlogPosts = cursor.getCount();
+
+        cursor.close();
+        database.close();
+
+        return numberOfBlogPosts;
     }
 }
